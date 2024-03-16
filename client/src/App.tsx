795519e140bed2b82fcd3f0b410a1dc087da5760
Maciej Fim <maciek_fim_99@o2.pl>
2024-03-16 20:40:56 +0100

    Add sidebar component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import LoginPage from "./Components/LoginPage/LoginPage";
 import RegisterPage from "./Components/RegisterPage/RegisterPage";
+import SideMenu from "./Components/Sidebar/Siedebar";
 
 function App() {
   return (
     <div className="App">
+      {/* TODO: dodać routing dla tych podstron oraz stworzyc policyTerms dla rejestracji */}
       {/* <LoginPage></LoginPage> */}
-      <RegisterPage></RegisterPage>
+      {/* <RegisterPage></RegisterPage> */}
+      <SideMenu></SideMenu>
     </div>
   );
 }
---
+++ client/src/Components/Sidebar/Siedebar.tsx
@@ -0,0 +1,107 @@
+function SideMenu() {
+    // TODO: zrobić componenty zeby był reuzywalne
+  return (
+    <div className="flex flex-col items-center w-64 h-screen overflow-hidden text-gray-400 bg-gray-900 rounded-r-xl">
+      <a className="flex items-center w-full px-3 mt-3" href="#">
+        <svg
+          className="w-8 h-8 fill-current"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" />
+        </svg>
+        <span className="ml-2 text-sm font-bold">100 ASSETS</span>
+      </a>
+      <div className="w-full px-2">
+        <div className="flex flex-col items-center w-full mt-3 border-t border-gray-700">
+          <a
+            className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300"
+            href="#"
+          >
+            <svg
+              className="w-6 h-6 stroke-current"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+              />
+            </svg>
+            <span className="ml-2 text-sm font-medium">Dasboard</span>
+          </a>
+
+          <a
+            className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300"
+            href="#"
+          >
+            <svg
+              className="w-6 h-6 stroke-current"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2"
+              />
+            </svg>
+            <span className="ml-2 text-sm font-medium">Guides</span>
+          </a>
+        </div>
+        <div className="flex flex-col items-center w-full mt-2 border-t border-gray-700">
+          <a
+            className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300"
+            href="#"
+          >
+            <svg
+              className="w-6 h-6 stroke-current"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+              />
+            </svg>
+            <span className="ml-2 text-sm font-medium">Inventory</span>
+          </a>
+        </div>
+      </div>
+      <a
+        className="flex items-center justify-center w-full h-16 mt-auto bg-gray-800 hover:bg-gray-700 hover:text-gray-300"
+        href="#"
+      >
+        <svg
+          className="w-6 h-6 stroke-current"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+        <span className="ml-2 text-sm font-medium">Account</span>
+      </a>
+    </div>
+  );
+}
+
+export default SideMenu;
