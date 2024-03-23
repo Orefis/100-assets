5152b949c8534cfcafa7c49dcda53dab64366a91
Maciej Fim <maciek_fim_99@o2.pl>
2024-03-23 17:45:16 +0100

    Refactor Sidebar for better reusability

--- client/src/Data/MenuItems.tsx
+++ client/src/Data/MenuItems.tsx
@@ -1,14 +1,13 @@
-interface MenuItem {
-    name: string;
-    path: string;
-    icon: string;
-  }
-  
-  const menuItems: MenuItem[] = [
-    { name: "Dashboard", path: "/home", icon: "dashboard-icon-path" },
-    { name: "Guides", path: "/guides", icon: "guides-icon-path" },
-    { name: "Inventory", path: "/inventory", icon: "inventory-icon-path" },
+export interface MenuItem {
+  name: string;
+  path: string;
+  icon: string;
+}
 
-  ];
-  
-  export default menuItems;
+const MenuItems: MenuItem[] = [
+  { name: "Dashboard", path: "/home", icon: "AiFillHome" },
+  { name: "Guides", path: "/guides", icon: "AiFillBook" },
+  { name: "Inventory", path: "/inventory", icon: "AiFillDatabase" },
+];
+
+export default MenuItems;
