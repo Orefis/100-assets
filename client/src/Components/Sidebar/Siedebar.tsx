7f22a9f9b745bd1d8b06bdc48b19efdc468939bc
Maciej Fim <maciek_fim_99@o2.pl>
2024-03-24 21:45:53 +0100

    Improve sidebar functions

--- client/src/Components/Sidebar/Siedebar.tsx
+++ client/src/Components/Sidebar/Siedebar.tsx
@@ -1,8 +1,7 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import MenuItems, { MenuItem } from '../../Data/MenuItems';
 import * as Icons from 'react-icons/ai';
 import { AiOutlineLogout, AiFillGold } from 'react-icons/ai';
-
 
 function SideMenu() {
   const IconComponent = (iconName: string) => {
@@ -11,36 +10,36 @@ function SideMenu() {
   };
 
   return (
-    <div className="flex flex-col items-center w-64 h-screen overflow-hidden text-gray-400 bg-gray-900 rounded-r-xl">
-      <Link
-        to="/"
-        className="flex items-center justify-center w-full h-16 bg-gray-800 hover:bg-gray-700 hover:text-gray-300"
+    <div className="flex flex-col items-center w-52 h-screen overflow-hidden text-gray-400 bg-gray-900 rounded-r-lg">
+      <NavLink
+        to="/home"
+        className={({ isActive }) => isActive ? "flex items-center justify-center w-full h-16 bg-gray-800 hover:bg-gray-700 hover:text-gray-300 border-b-2 border-gray-700 bg-gray-700" : "flex items-center justify-center w-full h-16 bg-gray-800 hover:bg-gray-700 hover:text-gray-300 border-b-2 border-gray-700"}
       >
-         <AiFillGold className="w-8 h-8" />
+        {/* TODO: przemyśleć sprawe z NAVLINK nt. headera w sidebarze */}
+        <AiFillGold className="w-6 h-6" />
         <span className="ml-2 text-lg font-medium">100 ASSETS</span>
-      </Link>
+      </NavLink>
       <div className="w-full px-3">
         {MenuItems.map((item: MenuItem) => (
-          <Link
+          <NavLink
             to={item.path}
             key={item.name}
-            className="flex flex-col items-center w-full mt-3 border-gray-700"
+            className={({ isActive }) => isActive ? "flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300 bg-gray-700 text-gray-300" : "flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300"}
           >
-            <div className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300">
-              <div style={{ fontSize: '24px' }}>{IconComponent(item.icon)}</div>
-              <span className="ml-2 text-base font-medium">{item.name}</span>
+            <div className="flex items-center w-full h-12">
+              <div style={{ fontSize: '18px' }}>{IconComponent(item.icon)}</div>
+              <span className="ml-2 text-sm font-medium">{item.name}</span>
             </div>
-          </Link>
-          // TODO: poprawic wielkosc ikon
+          </NavLink>
         ))}
       </div>
-      <Link
-        to="/"
-        className="flex items-center justify-center w-full h-16 mt-auto bg-gray-800 hover:bg-gray-700 hover:text-gray-300"
+      <NavLink
+        to="/logout"
+        className={({ isActive }) => isActive ? "flex items-center justify-center w-full h-16 mt-auto bg-gray-800 hover:bg-gray-700 hover:text-gray-300 bg-gray-700" : "flex items-center justify-center w-full h-16 mt-auto bg-gray-800 hover:bg-gray-700 hover:text-gray-300"}
       >
-         <AiOutlineLogout className="w-6 h-6" />
+        <AiOutlineLogout className="w-6 h-6" />
         <span className="ml-2 text-sm font-medium">Logout</span>
-      </Link>
+      </NavLink>
     </div>
   );
 }
